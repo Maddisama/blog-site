dffa8c48f3565340eca2a8bdf0afee2a1ccdf673
Madison <madisoncolemit@gmail.com>
2023-06-25 03:43:58 -0400

    read more fix

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Menu = ({ cat }) => {
   const [posts, setPosts] = useState([]);
@@ -44,7 +45,9 @@ const Menu = ({ cat }) => {
         <div className="post" key={post.id}>
           <img src={`../upload/${post?.img}`} alt="post" />
           <h2>{post.title}</h2>
-          <button>Read More</button>
+          <Link className="link" to={`/post/${post.id}`}>
+            <button>Read More</button>
+          </Link>
         </div>
       ))}
     </div>
